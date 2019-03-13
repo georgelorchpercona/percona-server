edea1c99ca4fed03a503a06a85ec537cb7bb11a7
George O. Lorch III <george.lorch@percona.com>
2019-03-13 15:06:55 -0700

    GR + MyRocks hack

--- plugin/group_replication/src/observer_trans.cc
+++ plugin/group_replication/src/observer_trans.cc
@@ -147,7 +147,8 @@ int group_replication_trans_before_dml(Trans_param *param, int &out) {
     - It should not contain 'ON DELETE/UPDATE CASCADE' referential action
    */
   for (uint table = 0; out == 0 && table < param->number_of_tables; table++) {
-    if (param->tables_info[table].db_type != DB_TYPE_INNODB) {
+    if (param->tables_info[table].db_type != DB_TYPE_INNODB &&
+        param->tables_info[table].db_type != DB_TYPE_ROCKSDB) {
       LogPluginErr(ERROR_LEVEL, ER_GRP_RPL_NEEDS_INNODB_TABLE,
                    param->tables_info[table].table_name);
       out++;
